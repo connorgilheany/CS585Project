cf55a9d8cc4b197e569a452e2540fbb6d1766b17
connorgilheany <connorgilheany@gmail.com>
2017-11-09 13:34:11 -0500

    Initial Commit

---
+++ Source.py
@@ -0,0 +1,49 @@
+"""
+
+CS 585 Fall '17 Project
+
+Authors: Connor Gilheany
+
+"""
+
+
+import numpy as np
+import cv2
+
+
+def main():
+    images = loadImages()
+
+    for image in images:
+        #cv2.imshow('image', batImage)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def loadImages():
+    return None
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
